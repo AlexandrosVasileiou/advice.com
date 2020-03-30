3b023bc71a6429f0ed340dcd3a162e34e0495c6a
Rene Gens <renegens@gmail.com>
2020-03-30 12:10:33 +0300

    Enable formatting inside json

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,12 +1,16 @@
 import React from "react"
 import "./post.scss"
 
+function createMarkup(content) {
+  return { __html: content }
+}
+
 const Post = (props) => {
   return (
     <article>
       <h2>{props.post.title}</h2>
       <h3>{props.post.tag}</h3>
-      <p>{props.post.content}</p>
+      <p dangerouslySetInnerHTML={createMarkup(props.post.content)}/>
     </article>
   )
 }
